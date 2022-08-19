f336b6991b0ea125708930563f78ba419ff73fdf
n9delta <n9delta@mail.ru>
2022-08-19 19:15:52 +0300

    Init commit

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong.'),
-	async execute(client, ia) {
+	async execute(client, i) {
 		return i.reply('Pong!');
 	},
 };
