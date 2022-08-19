6b1ae7f64ccabd75a5451fb790130e64a17585c0
n9delta <n9delta@mail.ru>
2022-08-19 21:02:33 +0300

    Добавлена отправка эмбидов с балансами кошельком

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,13 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('balance')
 		.setDescription('Shows wallets balance'),
 	async execute(client, i, qiwi) {
-		let balances = await qiwi.getTransHistory();
+		let wallets = await qiwi.getTransHistory();
 
-		console.log(balances);
-		i.reply('sux');
+		let embeds = [];
+		for (const wal of wallets.accounts) {
+			let embed = new MessageEmbed()
+				.setColor('#FF8C00')
+				.setTitle(`<:qiwi:1010262149903437885> ${wal.bankAlias} | ${wal.title}`)
+				.setDescription(`${wal.balance.amount} ${qiwi.currencyCodes[wal.balance.currency]}`);
+
+			embeds.push(embed);
+		}
+
+		i.reply({ embeds: embeds });
 	},
 };
