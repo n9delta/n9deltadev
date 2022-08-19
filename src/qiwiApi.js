cfca6df6f626453b5c9bb975a62759d08506d9c6
n9delta <n9delta@mail.ru>
2022-08-19 20:30:43 +0300

    init qiwi api

---
+++ src/qiwiApi.js
@@ -0,0 +1,62 @@
+const axios = require('axios').default;
+
+
+class Qiwi {
+	constructor(apiKey, phone) {
+		this.apiKey = apiKey;
+		this.phone = phone;
+
+		this.url = 'https://edge.qiwi.com';
+
+		this.headers = {
+			Accept: 'application/json',
+			content-type: 'application.json',
+			Authorization: 'Bearer' + apiKey,
+		};
+
+		this.currencyCodes = {
+			643: 'RUB',
+			840: 'USD',
+			978: 'EUR', 
+			398: 'KZT',
+		};
+	}
+
+	async get(options) {
+		if (!options.headers) options.headers = this.headers;
+
+		try {
+			let result = await axios.get(options.url, options);
+
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	async post(options) {
+		if (!options.headers) options.headers = this.headers;
+
+		try {
+			let result = await axios.post(options.url, options.body, options);
+
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	async put(options) {
+		if (!options.headers) options.headers = this.headers;
+
+		try {
+			let result = await axios.put(options.url, options.body, options);
+
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	}
+}
+
+module.exports = { Qiwi };
